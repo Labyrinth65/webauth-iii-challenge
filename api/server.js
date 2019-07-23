ad0556e982a3b1802ccfbc070016bf2aac288bf5
Ang Xu <angxu1234@gmail.com>
2019-07-23 16:58:13 -0500

    more specific error checking

--- api/server.js
+++ api/server.js
@@ -16,37 +16,37 @@ server.get("/", (req, res) => {
 	res.send(`<h2>WebAuth I Challenge</h2>`);
 });
 
-server.post("/api/register", (req, res) => {
-	let user = req.body;
-	const hash = bcrypt.hashSync(user.password, 8);
-	user.password = hash;
-
-	usersDB
-		.add(user)
-		.then(saved => {
-			res.status(201).json(saved);
-		})
-		.catch(error => {
-			res.status(500).json(error);
-		});
+server.post("/api/register", async (req, res) => {
+	try {
+		if (req.body.username && req.body.password) {
+			const hash = bcrypt.hashSync(req.body.password, 8);
+			req.body.password = hash;
+			const user = await usersDB.add(req.body);
+			res.status(201).json(user);
+		} else {
+			res
+				.status(400)
+				.json({ message: "please provide username and password." });
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error: "something went wrong." });
+	}
 });
 
-server.post("/api/login", (req, res) => {
-	let { username, password } = req.body;
-
-	usersDB
-		.findBy({ username })
-		.first()
-		.then(user => {
-			if (user && bcrypt.compareSync(password, user.password)) {
-				res.status(200).json({ message: `Welcome ${user.username}!` });
-			} else {
-				res.status(401).json({ message: "Invalid Credentials" });
-			}
-		})
-		.catch(error => {
-			res.status(500).json(error);
-		});
+server.post("/api/login", async (req, res) => {
+	try {
+		let { username, password } = req.body;
+		const user = await usersDB.findBy({ username }).first();
+		if (user && bcrypt.compareSync(password, user.password)) {
+			res.status(200).json({ message: `Welcome ${user.username}!` });
+		} else {
+			res.status(401).json({ message: "Invalid Credentials" });
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error: "something went wrong." });
+	}
 });
 
 server.use(express.json());
